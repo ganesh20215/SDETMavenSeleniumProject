f7ac0fc183b034708e8baa1d5ebc7027f6ed125e
Ganesh Jadhav <ganesh20215@gmail.com>
2023-10-04 21:49:46 +0530

    Check Test 2

--- src/test/java/demo/FirstTestNgProgram.java
+++ src/test/java/demo/FirstTestNgProgram.java
@@ -17,15 +17,6 @@ public class FirstTestNgProgram {
     }
 
     @Test
-    public void testCase2() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Ganesh\\Downloads\\4Sept2023\\chromedriver-win64 (1)\\chromedriver-win64\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();  //UpCasting
-        driver.navigate().to("https://www.amazon.in/");
-        driver.manage().window().maximize();
-        driver.close();
-    }
-
-    @Test
     public void testCase4() {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\Ganesh\\Downloads\\4Sept2023\\chromedriver-win64 (1)\\chromedriver-win64\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();  //UpCasting
