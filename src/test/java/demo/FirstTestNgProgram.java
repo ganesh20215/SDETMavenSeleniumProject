1e14ccdb70d69f16ad3f4b8ff8f2d6812eb7debd
Ganesh Jadhav <ganesh20215@gmail.com>
2023-10-04 21:46:53 +0530

    check test 1

--- src/test/java/demo/FirstTestNgProgram.java
+++ src/test/java/demo/FirstTestNgProgram.java
@@ -24,13 +24,4 @@ public class FirstTestNgProgram {
         driver.manage().window().maximize();
         driver.close();
     }
-
-    @Test
-    public void testCase4() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Ganesh\\Downloads\\4Sept2023\\chromedriver-win64 (1)\\chromedriver-win64\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();  //UpCasting
-        driver.navigate().to("https://www.flipkart.com/");
-        driver.manage().window().maximize();
-        driver.close();
-    }
 }
